2f24ae69da8916862dcc969b30b4bf065d2eab5e
Hendrik Purmann <h.purmann@googlemail.com>
2019-10-08 14:48:07 +0200

    Add simple routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
-import axios from 'axios';
+import express from 'express'
+import bodyParser from 'body-parser'
+import createLogger from '@rplan/logger'
 import config from '@rplan/config'
+import { router } from './routes.js'
 
-const externalService = config.get('external_service')
+const logger = createLogger('server')
 
-console.log('externalService:', externalService)
+// const externalService = config.get('external_service')
+
+export async function initApp() {
+  const app = express()
+  app.disable('x-powered-by')
+
+  app.use(bodyParser.json())
+  app.use(router)
+
+  return app
+}
+
+function shutdownServer(server) {
+  logger.info('shutting down server')
+  server.close(() => {
+    logger.info('server was shut down')
+  })
+}
+
+
+async function start() {
+  const app = await initApp()
+
+  const port = config.get('server:port')
+
+  const server = app.listen(port, () => {
+    logger.info(`simple-node-app listening on port ${port}`)
+  })
+
+  process.on('SIGINT', () => {
+    shutdownServer(server)
+  })
+
+}
+
+if (require.main === module) {
+  start()
+    .catch((err) => {
+      logger.error(err)
+      process.exit(1)
+    })
+}
+
